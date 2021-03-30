f411a68246a6b3dc49c58906ee2850de27b04fa7
bhannawa <brandonhannawa@gmail.com>
2021-03-30 02:03:55 -0400

    Lots

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
-static int borderpx = 2;
+static char *font = "mono:pixelsize=24:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=28:antialias=true:autohint=true" };
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
